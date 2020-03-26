23bcd32290b0c08387581adda8ee99f62b9ddfa8
joakim persson <joakim.persson@findity.com>
2020-03-26 11:23:12 +0100

    first interface video

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/smakaroni/golang-arch
+
+go 1.12
---
+++ main.go
@@ -0,0 +1,28 @@
+package main
+
+import "fmt"
+
+type person struct {
+	first string
+}
+
+func (p person) speak() {
+	fmt.Println("from person - this is my name", p.first)
+}
+
+//any TYPE that has the methods specified by an interface is also of the interface type
+
+type human interface {
+	speak()
+}
+
+func main() {
+	p1 := person{first:"Emma"}
+
+	fmt.Printf("%T\n", p1)
+
+	var x human
+	// only works because person has the method speak()
+	x = p1
+	fmt.Printf("%T\n", x)
+}
